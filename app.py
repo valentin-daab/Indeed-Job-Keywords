534d9b44e46730440071c10be21a1a77d9489d61
bladee-d9 <daab.valentin@icloud.com>
2022-05-03 18:56:03 +0200

    v1

--- app.py
+++ app.py
@@ -1,45 +1,52 @@
-from nturl2path import url2pathname
 from flask import (Flask, render_template, request, flash)
+from bs4 import BeautifulSoup
+import requests
 import re
 import spacy
-import requests
 from typing import Counter
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.secret_key = 'dd hh'
 
-@app.route('/', methods=("GET", "POST"))
+
+@app.route('/', methods=("GET", "POST"), strict_slashes=False)
 def home():
     if request.method == "POST":
-
         try :
-            global requested_url
 
-            req = requests.get(requested_url)
-            soup = BeautifulSoup(req.text, 'html.parser')
+            global req
+
+            req = request.form.get("requested_url")
+
+            html_content = requests.get(str(req))
+            soup = BeautifulSoup(html_content.text, 'html.parser')
             contents = soup.find(class_ = "jobsearch-jobDescriptionText")
-
 
             contents = re.sub('<br/>','. ',str(contents))
             contents = re.sub('<[^<]+?>', ' ', str(contents))
             contents = re.sub('\&amp;', ' / ', str(contents))
-
+            contents = re.sub('- ', '', str(contents))
 
             nlp = spacy.load("fr_core_news_md")
             doc = nlp(contents)
             nouns = [token.lemma_ for token in doc if token.pos_ == "NOUN"]
-            nouns_freq = Counter(nouns)
-            print(nouns_freq.most_common(10))
+            nouns_freq = Counter(nouns).most_common(5)
 
-            return render_template("index.html")
+            verbs = [token.lemma_ for token in doc if token.pos_ == "VERB"]
+            verbs_freq = Counter(verbs).most_common(5)
+
+            n_labels = [row[0] for row in nouns_freq]
+            n_values = [row[1] for row in nouns_freq]
+
+            v_labels = [row[0] for row in verbs_freq]
+            v_values = [row[1] for row in verbs_freq]
+
+            return render_template("index.html", n_labels=n_labels, n_values=n_values, v_labels=v_labels, v_values=v_values)
 
         except Exception as e:
-            flash(e, "danger")
+            flash(str(e), "danger")
 
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.secret_key = 'super secret key'
-    app.debug = True
+    app.run(debug=True,host='0.0.0.0')
